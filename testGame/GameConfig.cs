83a5824ed938b6ac66e34cc9e15448a3c4607c82
vic <finalsteve@hotmail.com>
2017-01-20 13:47:22 +0800

    update

--- testGame/GameConfig.cs
+++ testGame/GameConfig.cs
@@ -13,11 +13,12 @@ class GameConfig
     public static List<object[]> WeaponConfig = new List<object[]>()
     {
         /* 0_name, 1_age, 2_size, 3_dragable, 4_count, 5_offset, 6_expand_speed, 7_delay, 8_startSize, 9_clearWhenRelease, 10_auto, 11_shootingTime, 11_using in test */
-        new object[] { "步槍(半自動)", 10, .6f, false, 1, 30.0f, 0.5f, false, 0.0f, false, false, 5, true },
+        new object[] { "步槍(半自動)", 10, .6f, false, 1, 30.0f, 0.5f, false, 0.0f, false, false, 5, false },
         new object[] { "高性能狙擊槍", 60, 1.0f, false, 1, 0.0f, 0.1f, false, 1.0f, true, false, 0, false },
         new object[] { "雙管散彈槍", 6, 1.0f, false, 10, 20.0f, 1.0f, false, 0.0f, false, false, 0, false },
-        new object[] { "智慧型狙擊槍", 300, 1.0f, true, 1, 0.0f, 0.2f, true, .5f, true, false, 0, true },
+        new object[] { "智慧型狙擊槍", 300, 1.0f, true, 1, 0.0f, 0.2f, true, .5f, true, false, 0, false },
         new object[] { "雷射加農砲", 300, 1.0f, true, 1, 0.0f, 0.1f, true, 0.0f, true, false, 0, false },
+        new object[] { "砍刀:半自動", 10, 1.0f, false, 1, 0.0f, 0.03f, false, .7f, true, false, 0, true },
         new object[] { "步槍:全自動", 6, 1.0f, false, 1, 30.0f, 0.2f, false, 0.0f, false, true, 0, false },
         new object[] { "雷射機槍", 6, 1.0f, false, 1, 10.0f, 0.2f, false, 0.0f, false, true, 0, false },
         new object[] { "衝鋒槍", 6, 1.0f, false, 1, 15.0f, 0.2f, false, 0.0f, false, true, 0, false },
---
+++ testGame/test/TestAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKeyUp( KeyCode.Space))
+        {
+            GetComponent<Animator>().Stop();
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            GetComponent<Animator>().enabled = false;
+            GetComponent<Animator>().enabled = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            GetComponent<Animator>().enabled = false;
+            GetComponent<Animator>().enabled = true;
+            GetComponent<Animator>().Play("Blade_right");
+        }
+    }
+}
