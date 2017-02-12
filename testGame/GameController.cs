e130dc3764719015f20f726c53804557da1a1b0f
vic <finalsteve@hotmail.com>
2017-02-12 23:40:59 +0800

    update

--- testGame/GameController.cs
+++ testGame/GameController.cs
@@ -121,15 +121,7 @@ public class GameController : MonoBehaviour {
         GetComponent<WongGestureController>().OnTwoFingerClicked += OnTwoFingerClicked;
         GetComponent<WongGestureController>().OnTwoFingerFlicked += OnTwoFingerFlicked;
         GetComponent<WongGestureController>().OnTwoFingerMove += OnTwoFingerMove;
-
-        /*
-        vc.Player.GetComponent<TapGesture>().Tapped += OnPlayerTapped;
-        vc.GamePage.GetComponent<LongPressGesture>().LongPressed += OnGamePageLongPressed;
-
-        vc.GamePage.GetComponent<Button>().onClick.AddListener(OnGamePageClick);
-        vc.GamePage.GetComponent<FlickGesture>().Flicked += OnGamePageFlicked;
-        vc.GamePage.GetComponent<DoubleFlickedGesture>().OnDoubleFlickedEvent += OnGamePageDoubleFlicked;
-        */
+        
         ReStart();
 
     }
@@ -190,124 +182,10 @@ public class GameController : MonoBehaviour {
         vc.CreateEnemy(pos);
     }
 
-    private void OnGamePageLongPressed(object sender, EventArgs e)
-    {
-#if UNITY_EDITOR
-#else
-        if ( Input.touchCount == 1 )
-        {
-#endif
-            Vector3 touchPos = vc.GamePage.GetComponent<LongPressGesture>().ScreenPosition;
-            Vector3 firePos = Camera.main.ScreenToWorldPoint(touchPos);
-            foreach (IWeapon w in vc.Player.weapons) w.StartAim(firePos);
-#if UNITY_EDITOR
-#else
-        }
-        else if ( Input.touchCount == 2 ){
-            
-            isDoubleHold = true;
-           // uc.SetState("OnGamePageLongPressed: isDoubleHold: " + isDoubleHold);
-        }
-#endif
-        uc.SetState("OnGamePageLongPressed");
-    }
-
-    private void OnGamePageClick()
-    {
-        isDoubleHold = false;
-
-        //uc.SetState("OnGamePageReleased: isDoubleHold: " + isDoubleHold);
-        foreach (IWeapon w in vc.Player.weapons) w.EndAim();
-
-        if (StopPlayerWhenTowFingerRelease)
-        {
-            if (GetTouchCount() == 2)
-            {
-                vc.Player.StopMove();
-                uc.SetState("StopMove");
-            }
-        }
-    }
-
-    private void OnGamePagePressed(object sender, EventArgs e)
-    {
-#if UNITY_EDITOR
-#else
-        if ( Input.touchCount == 1)
-        {
-#endif
-        Vector3 touchPos = vc.GamePage.GetComponent<PressGesture>().ScreenPosition;
-        Vector3 firePos = Camera.main.ScreenToWorldPoint(touchPos);
-        foreach (IWeapon w in vc.Player.weapons) w.StartAim(firePos);
-#if UNITY_EDITOR
-#else
-        }
-#endif
-        uc.SetState("OnGamePagePressed");
-    }
-
-    private void OnGamePageDoubleFlicked(FlickGesture obj)
-    {
-#if UNITY_EDITOR
-        DodgePlayer(obj.ScreenFlickVector.normalized);
-#else
-        if ( GetTouchCount() == 2)
-        {
-            DodgePlayer( obj.ScreenFlickVector.normalized );
-        }
-#endif
-    }
-
     void DodgePlayer(Vector3 dir)
     {
         uc.SetState("Dodge");
         vc.Player.DodgePlayer(dir, GameConfig.DodgeSpeed);
-
-        /* 因為此操作會和單指操作衝突，因此加個flag來決定單指操作是否能觸發 */
-        isDoubleFlicked = true;
-        StartCoroutine(DelayCall(.3f, () =>
-        {
-            isDoubleFlicked = false;
-        }));
-    }
-
-    private void OnGamePageFlicked(object sender, EventArgs e)
-    {
-#if UNITY_EDITOR
-        if (!isDoubleFlicked)
-        {
-            Vector3 dir = vc.GamePage.GetComponent<FlickGesture>().ScreenFlickVector.normalized;
-            Vector3 fromVec = vc.Player.GetComponent<RectTransform>().position;
-            vc.Player.SetPlayerPosition(fromVec + dir * GameConfig.LongMoveDistance);
-
-            uc.SetState("Move Long Distance:" + dir * GameConfig.LongMoveDistance);
-
-            /* 因為此操作會和雙指持續按壓地面的操作衝突，因此加個flag來決定雙指持續按壓地面的操作是否能觸發 */
-            isFlicked = true;
-            StartCoroutine(DelayCall(.6f, () =>
-            {
-                isFlicked = false;
-            }));
-        }
-
-#else
-        if ( GetTouchCount() == 2 && !isDoubleFlicked )
-        {
-            Vector3 dir = vc.GamePage.GetComponent<FlickGesture>().ScreenFlickVector.normalized;
-            Vector3 fromVec = vc.Player.GetComponent<RectTransform>().position;
-            vc.Player.SetPlayerPosition(fromVec + dir * GameConfig.LongMoveDistance);
-
-            uc.SetState("Move Long Distance:" + dir * GameConfig.LongMoveDistance);
-
-            /* 因為此操作會和雙指持續按壓地面的操作衝突，因此加個flag來決定雙指持續按壓地面的操作是否能觸發 */
-            isFlicked = true;
-            StartCoroutine(DelayCall( .6f, () =>
-            {
-                isFlicked = false;
-            }));
-        }
-#endif
-
     }
 
     private void OnEnemyTapped(object sender, EventArgs e)
@@ -326,7 +204,7 @@ public class GameController : MonoBehaviour {
         vc.Player.MakePlayerStop();
         uc.SetState("Stop Player");
     }
-
+    /*
     private void OnPlayerTapped(object sender, EventArgs e)
     {
         if ( GetTouchCount() == 2 && GetIsClick() )
@@ -335,7 +213,7 @@ public class GameController : MonoBehaviour {
             vc.Player.MakePlayerStop();
         }
     }
-    
+    */
     private void OnTapped(object sender, EventArgs e)
     {
         uc.SetState("OnGroundTapped");
@@ -357,39 +235,6 @@ public class GameController : MonoBehaviour {
         vc.Player.SetPlayerPositionByScreenPos( Input.mousePosition );
     }
 
-    IEnumerator DelayCall( float time, Action doAction )
-    {
-        yield return new WaitForSeconds( time );
-        doAction();
-    }
-
-    int GetTouchCount()
-    {
-        return GetComponent<DetectTouchCountByPassTime>().TouchCount;
-    }
-
-    bool GetIsClick()
-    {
-        return GetComponent<DetectTouchCountByPassTime>().IsClick;
-    }
-
-    Vector3 GetTouchPosition()
-    {
-        return GetComponent<DetectTouchCountByPassTime>().GetAverageTouchPosition();
-    }
-
-    Vector3 GetLastTouchPosition()
-    {
-        Vector2 pos = new Vector2();
-        int touchCount = Input.touchCount;
-        foreach( Touch t in Input.touches)
-        {
-            pos += t.position;
-        }
-        pos /= touchCount;
-        return new Vector3(pos.x, pos.y, 0);
-    }
-
     void FireOnce( Vector3 firePos )
     {
         Vector3 fromVec = vc.Player.GetComponent<RectTransform>().position;
@@ -403,155 +248,15 @@ public class GameController : MonoBehaviour {
         Vector3 dir = (firePos - fromVec).normalized;
         vc.CreateSpecialBullet(fromVec + dir * 50, dir, 100);
     }
-
-    int judgeIsHoldTime = 0;
     
     void Update () {
-
-        return;
-
-        /* 是點擊、非swipe、非連續swipe、兩指在屏上才會觸發 */
-	    if( GetTouchCount() == 2 && GetIsClick() && !isFlicked && !isDoubleFlicked)
+        /*
+        if (EnableShadow)
         {
-            vc.Player.SetPlayerPositionByScreenPos(GetTouchPosition());
-            uc.SetState( "Normal Move" );
+            if (isDoubleFlicked) vc.CreateShadow(vc.Player.Position, vc.Player.Scale);
         }
-
-        /* 如果單指有劃過，就不觸動持續移動的功能 */
-        if ( isDoubleHold && !isFlicked && !isDoubleFlicked)
-        {
-            vc.Player.SetPlayerPositionByScreenPos(GetLastTouchPosition());
-            uc.SetState("Normal Move");
-        }
-
-        if( EnableShadow )
-        {
-            if(isDoubleFlicked) vc.CreateShadow(vc.Player.Position, vc.Player.Scale);
-        }
-
-        //uc.SetState("isDoubleHold: " + isDoubleHold);
-        //uc.SetState(GetComponent<DetectTouchCountByPassTime>().GetTouchString());
-        //uc.SetState("judgeIsHoldTime: " + judgeIsHoldTime.ToString());
-
-#if UNITY_EDITOR
-        foreach (IWeapon w in vc.Player.weapons) w.MoveAim(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        foreach (IWeapon w in vc.Player.weapons) w.KeepStartAim(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-#else
-        if (GetTouchCount() == 1 )
-        {
-            foreach (IWeapon w in vc.Player.weapons) w.MoveAim(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            if( GetIsClick() )
-            {
-                if( !isClicked ){
-                    isClicked = true;
-                    judgeIsHoldTime = 0;
-                    StartCoroutine(DelayCall(.3f, () =>
-                    {
-                        isClicked = false;
-                    }));
-
-                    Vector3 firePos = Camera.main.ScreenToWorldPoint(GetTouchPosition());
-                    foreach (IWeapon w in vc.Player.weapons) w.AimOnce(firePos);
-                }
-            }
-            else
-            {
-                judgeIsHoldTime++;
-                if( judgeIsHoldTime >= 5 ){
-                    Vector3 firePos = Camera.main.ScreenToWorldPoint(GetTouchPosition());
-                    foreach (IWeapon w in vc.Player.weapons) w.KeepStartAim(firePos);
-                }
-            }
-        }else{
-            judgeIsHoldTime = 0;
-        }
-#endif
+        */
         foreach (IWeapon w in vc.Player.weapons) w.Update();
-    }
-    /*
-    void OnGUI()
-    {
-        CreateConfigGUI();
-    }*/
-
-    void CreateConfigGUI()
-    {
-        if (showConfig)
-        {
-            GUIStyle style = new GUIStyle();
-#if UNITY_EDITOR
-            style.fontSize = 10;
-            style.fixedWidth = 40;
-#else
-            style.fontSize = 30;
-            style.fixedWidth = 140;
-#endif
-            style.margin = new RectOffset(10, 10, 5, 5);
-            style.normal.textColor = Color.white;
-            style.alignment = TextAnchor.LowerRight;
-
-            GUILayout.BeginHorizontal();
-            GUILayout.TextField("移動速度", style);
-            GUILayout.TextField("閃避速度", style);
-            GUILayout.TextField("長距離移動", style);
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GameConfig.MoveSpeed = float.Parse(GUILayout.TextField(GameConfig.MoveSpeed + "", style));
-            GameConfig.DodgeSpeed = float.Parse(GUILayout.TextField(GameConfig.DodgeSpeed + "", style));
-            GameConfig.LongMoveDistance = int.Parse(GUILayout.TextField(GameConfig.LongMoveDistance + "", style));
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.TextField("武器名稱", style);
-            GUILayout.TextField("準星生命", style);
-            GUILayout.TextField("準星大小", style);
-            GUILayout.TextField("可拖動", style);
-            GUILayout.TextField("數量", style);
-            GUILayout.TextField("偏移量", style);
-            GUILayout.TextField("擴張速度", style);
-            GUILayout.TextField("延遲射擊", style);
-            GUILayout.TextField("起使大小", style);
-            GUILayout.TextField("離開消失", style);
-            GUILayout.TextField("全/半自動", style);
-            GUILayout.TextField("時長", style);
-            GUILayout.TextField("刀裝備", style);
-            GUILayout.TextField("裝備", style);
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.EndHorizontal();
-
-            /* name, age, size, dragable, count, offset, expand_speed, delay, startSize, clearWhenRelease */
-
-            foreach (object[] c in GameConfig.WeaponConfig)
-            {
-                GUILayout.BeginHorizontal();
-                c[0] = GUILayout.TextField((string)c[0], style);
-                //c[1] = (int)GUILayout.HorizontalSlider((int)c[1], 0, 300);
-                c[1] = int.Parse(GUILayout.TextField(c[1] + "", style));
-                c[2] = float.Parse(GUILayout.TextField(c[2] + "", style));
-                c[3] = GUILayout.Toggle((bool)c[3], (bool)c[3] ? "可" : "不可", style);
-                c[4] = int.Parse(GUILayout.TextField(c[4] + "", style));
-                c[5] = float.Parse(GUILayout.TextField(c[5] + "", style));
-                c[6] = float.Parse(GUILayout.TextField(c[6] + "", style));
-                c[7] = GUILayout.Toggle((bool)c[7], (bool)c[7] ? "延遲" : "非延遲", style);
-                c[8] = float.Parse(GUILayout.TextField(c[8] + "", style));
-                c[9] = GUILayout.Toggle((bool)c[9], (bool)c[9] ? "消失" : "不消失", style);
-                c[10] = GUILayout.Toggle((bool)c[10], (bool)c[10] ? "全自" : "半自", style);
-                c[11] = int.Parse(GUILayout.TextField(c[11] + "", style));
-                c[12] = GUILayout.Toggle((bool)c[12], (bool)c[12] ? "是" : "否", style);
-                c[13] = GUILayout.Toggle((bool)c[13], (bool)c[13] ? "裝備" : "不裝備", style);
-                GUILayout.EndHorizontal();
-                GUILayout.BeginHorizontal();
-                GUILayout.EndHorizontal();
-            }
-        }
+        
     }
 }
--- testGame/WongGestureController.cs
+++ testGame/WongGestureController.cs
@@ -7,11 +7,8 @@ using UnityEngine.UI;
 using VicScript;
 
 public class WongGestureController : MonoBehaviour {
-
-   // public GameObject Player;
+    
     public GameObject TouchScreen;
-
-   // public Action OnStopPlayer;
     public Action<Vector3> OnOneFingerDown;
     public Action OnEachFingerUp;
     public Action<Vector3> OnOneFingerClicked;
@@ -27,44 +24,29 @@ public class WongGestureController : MonoBehaviour {
     bool isFlicked = false;
     bool isClicked = false;
     int judgeIsHoldTime = 0;
-
-    // Use this for initialization
+    
     void Start () {
-       // Player.GetComponent<TapGesture>().Tapped += OnPlayerTapped;
         TouchScreen.GetComponent<LongPressGesture>().LongPressed += OnGamePageLongPressed;
         TouchScreen.GetComponent<Button>().onClick.AddListener(OnGamePageClick);
         TouchScreen.GetComponent<FlickGesture>().Flicked += OnGamePageFlicked;
         TouchScreen.GetComponent<DoubleFlickedGesture>().OnDoubleFlickedEvent += OnGamePageDoubleFlicked;
     }
-	
-	// Update is called once per frame
+
 	void Update () {
         /* 是點擊、非swipe、非連續swipe、兩指在屏上才會觸發 */
         if (GetTouchCount() == 2 && GetIsClick() && !isFlicked && !isDoubleFlicked)
         {
-           // Player.SetPlayerPositionByScreenPos(GetTouchPosition());
-            // uc.SetState("Normal Move");
-
             if (OnTwoFingerClicked != null) OnTwoFingerClicked.Invoke( Camera.main.ScreenToWorldPoint(GetTouchPosition()));
         }
 
         /* 如果單指有劃過，就不觸動持續移動的功能 */
         if (isDoubleHold && !isFlicked && !isDoubleFlicked)
         {
-            //Player.SetPlayerPositionByScreenPos(GetLastTouchPosition());
             if (OnTwoFingerMove != null) OnTwoFingerMove.Invoke(Camera.main.ScreenToWorldPoint(GetTouchPosition()));
-            //  uc.SetState("Normal Move");
         }
-        /*
-        if (EnableShadow)
-        {
-            if (isDoubleFlicked) vc.CreateShadow(vc.Player.Position, vc.Player.Scale);
-        }
-        */
-        
+
         if (GetTouchCount() == 1 )
         {
-            //foreach (IWeapon w in vc.Player.weapons) w.MoveAim(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             if (OnOneFingerMove != null) OnOneFingerMove.Invoke(Camera.main.ScreenToWorldPoint(GetTouchPosition()));
             if ( GetIsClick() )
             {
@@ -76,73 +58,36 @@ public class WongGestureController : MonoBehaviour {
                         isClicked = false;
                     }));
                     if (OnOneFingerClicked != null) OnOneFingerClicked.Invoke(Camera.main.ScreenToWorldPoint(GetTouchPosition()));
-                   // Vector3 firePos = Camera.main.ScreenToWorldPoint(GetTouchPosition());
-                   // foreach (IWeapon w in vc.Player.weapons) w.AimOnce(firePos);
                 }
             }
             else
             {
                 judgeIsHoldTime++;
                 if( judgeIsHoldTime >= 5 ){
-                  //  Vector3 firePos = Camera.main.ScreenToWorldPoint(GetTouchPosition());
-                  //  foreach (IWeapon w in vc.Player.weapons) w.KeepStartAim(firePos);
-
                     if (OnOneFingerMoveAfterHold != null) OnOneFingerMoveAfterHold.Invoke(Camera.main.ScreenToWorldPoint(GetTouchPosition()));
                 }
             }
         }else{
             judgeIsHoldTime = 0;
         }
-       // foreach (IWeapon w in vc.Player.weapons) w.Update();
     }
-
-
-    private void OnPlayerTapped(object sender, EventArgs e)
-    {
-        if (GetTouchCount() == 2 && GetIsClick())
-        {
-           // if (OnStopPlayer != null) OnStopPlayer.Invoke();
-        }
-    }
-
 
     private void OnGamePageLongPressed(object sender, EventArgs e)
     {
         if ( Input.touchCount == 1 )
         {
-            /*
-            Vector3 touchPos = vc.GamePage.GetComponent<LongPressGesture>().ScreenPosition;
-            Vector3 firePos = Camera.main.ScreenToWorldPoint(touchPos);
-            foreach (IWeapon w in vc.Player.weapons) w.StartAim(firePos);
-            */
             Vector3 touchPos = TouchScreen.GetComponent<LongPressGesture>().ScreenPosition;
             if (OnOneFingerDown != null) OnOneFingerDown(Camera.main.ScreenToWorldPoint(touchPos));
         }
         else if ( Input.touchCount == 2 ){
-            
             isDoubleHold = true;
-           // uc.SetState("OnGamePageLongPressed: isDoubleHold: " + isDoubleHold);
         }
-      //  uc.SetState("OnGamePageLongPressed");
     }
 
     private void OnGamePageClick()
     {
         isDoubleHold = false;
         if (OnEachFingerUp != null) OnEachFingerUp.Invoke();
-        //uc.SetState("OnGamePageReleased: isDoubleHold: " + isDoubleHold);
-        /*
-        foreach (IWeapon w in vc.Player.weapons) w.EndAim();
-
-        if (StopPlayerWhenTowFingerRelease)
-        {
-            if (GetTouchCount() == 2)
-            {
-                vc.Player.StopMove();
-                uc.SetState("StopMove");
-            }
-        }
-        */
     }
 
     private void OnGamePageFlicked(object sender, EventArgs e)
