c6bb25cef4f1d975199c7e55e0094c672de22711
vic <finalsteve@hotmail.com>
2017-02-13 01:02:53 +0800

    afterGesture

---
+++ VicScript/WongWeaponSystem/AimViewController.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using System;
+using UnityEngine.UI;
+
+public class AimViewController : MonoBehaviour {
+    
+    public GameObject ObjectContainer;
+    public GameObject Prefab;
+
+    public Action<Vector3, object[]> OnCreateAim;
+    public Action<Vector3> OnDragAim;
+    public Action OnAimEmpty;
+
+    Dictionary<int,List<AimController>> Aims = new Dictionary<int,List<AimController>>();
+    int aimsId = 0;
+
+    public int CreateAim(PlayerController attacker, Vector3 pos, object[] config )
+    {
+        int age = (int)config[1];
+        float size = (float)config[2];
+        bool dragable = (bool)config[3];
+        int count = (int)config[4];
+        float seperateRange = (float)config[5];
+        float expandSpeed = (float) config[6];
+        bool delay = (bool)config[7];
+        float startSize = (float)config[8];
+        bool isBlade = (bool)config[12];
+
+        aimsId++;
+        if (!Aims.ContainsKey(aimsId))
+        {
+            Aims.Add(aimsId, new List<AimController>());
+        }
+
+        for (int i = 0; i < count; ++i)
+        {
+            GameObject aim = Instantiate(Prefab);
+            aim.SetActive(true);
+            aim.transform.parent = ObjectContainer.transform;
+            aim.GetComponent<AimController>().SetConfig(age, size, startSize, dragable, expandSpeed, delay);
+            Vector3 offset = new Vector3(UnityEngine.Random.Range(-1.0f, 1.0f) * seperateRange, UnityEngine.Random.Range(-1.0f, 1.0f) * seperateRange, 0);
+            aim.GetComponent<AimController>().owner = attacker;
+            aim.GetComponent<AimController>().Offset = offset;
+            aim.GetComponent<AimController>().SetPosition(pos);
+            aim.GetComponent<AimController>().GroupId = aimsId;
+            aim.GetComponent<AgeCalculator>().OnDeadEvent += OnAimDeadEvent;
+            Aims[aimsId].Add(aim.GetComponent<AimController>());
+        }
+
+        if (OnCreateAim != null)
+        {
+            OnCreateAim.Invoke(pos, config);
+        }
+        return aimsId;
+    }
+
+    private void OnAimDeadEvent(AgeCalculator obj)
+    {
+        obj.OnDeadEvent -= OnAimDeadEvent;
+        GameObject aimSender = obj.gameObject;
+        int groupId = aimSender.GetComponent<AimController>().GroupId;
+        List<AimController> nowAims = GetAimById(groupId);
+        if (nowAims != null)
+        {
+            nowAims.Remove(aimSender.GetComponent<AimController>());
+            Destroy(aimSender);
+
+            if (nowAims.Count == 0)
+            {
+                if (Aims.ContainsKey(groupId))
+                {
+                    Aims.Remove(groupId);
+                }
+            }
+        }
+        CheckIsStopAim();
+    }
+
+    public void DragAimsByIds(int[] ids, Vector3 pos)
+    {
+        foreach( int id in ids)
+        {
+            List<AimController> nowAims = GetAimById(id);
+            if (nowAims != null)
+            {
+                foreach (AimController a in nowAims)
+                {
+                    if (a.Dragable)
+                    {
+                        a.GetComponent<AimController>().SetPosition(pos);
+                        if (OnDragAim != null) OnDragAim.Invoke(pos);
+                    }
+                }
+            }
+        }
+    }
+
+    public List<AimController> GetAimById(int id)
+    {
+        if(Aims.ContainsKey(id))
+        {
+            return Aims[id];
+        }
+        return null;
+    }
+
+    public void ClearAimsByIds( int[] ids )
+    {
+        foreach( int id in ids)
+        {
+            List<AimController> nowAims = GetAimById(id);
+            if (nowAims != null)
+            {
+                foreach (AimController a in nowAims)
+                {
+                    a.GetComponent<AgeCalculator>().OnDeadEvent -= OnAimDeadEvent;
+                    Destroy(a.gameObject);
+                }
+                nowAims.Clear();
+                if (Aims.ContainsKey(id)) Aims.Remove(id);
+            }
+        }
+        CheckIsStopAim();
+    }
+    
+    void CheckIsStopAim()
+    {
+        if (Aims.Count == 0)
+        {
+            if (OnAimEmpty != null) OnAimEmpty.Invoke();
+        }
+    }
+    
+    void Start()
+    {
+        CheckIsStopAim();
+    }
+    
+    
+}
---
+++ VicScript/WongWeaponSystem/WongWeaponController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WongWeaponController : MonoBehaviour {
+
+    public bool IsAim = false;
+    public List<IWeapon> weapons = new List<IWeapon>();
+    /*
+    public void AddWeapon( object[] config )
+    {
+            bool autoWeapon = (bool)config[10];
+            if (autoWeapon)
+            {
+                weapons.Add(new AutoWeapon(config));
+            }
+            else
+            {
+                weapons.Add(new HalfAutoWeapon(config));
+            }
+    }
+    */
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
